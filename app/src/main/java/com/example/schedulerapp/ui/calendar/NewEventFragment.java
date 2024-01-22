aa9054c0988dff1b224f4f5c30f65d11deae1273
RolandHaden <davis.haden@gmail.com>
2024-01-22 00:12:05 -0500

    worked on new events and added a calendar selection for the date of an event

--- app/src/main/java/com/example/schedulerapp/ui/calendar/NewEventFragment.java
+++ app/src/main/java/com/example/schedulerapp/ui/calendar/NewEventFragment.java
@@ -1,10 +1,13 @@
 package com.example.schedulerapp.ui.calendar;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
@@ -12,6 +15,8 @@ import androidx.fragment.app.Fragment;
 
 import com.example.schedulerapp.R;
 import com.example.schedulerapp.databinding.FragmentNewEventBinding;
+
+import java.util.Calendar;
 
 
 /**
@@ -29,7 +34,6 @@ public class NewEventFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
 
     public NewEventFragment() {
         // Required empty public constructor
@@ -60,6 +64,7 @@ public class NewEventFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     private FragmentNewEventBinding binding;
@@ -71,17 +76,39 @@ public class NewEventFragment extends Fragment {
         binding = FragmentNewEventBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         Spinner spinner = (Spinner) root.findViewById(R.id.spinnerEvent);
-// Create an ArrayAdapter using the string array and a default spinner layout.
+        // Create an ArrayAdapter using the string array and a default spinner layout.
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 getContext(),
                 R.array.events,
                 android.R.layout.simple_spinner_item
         );
-// Specify the layout to use when the list of choices appears.
+        // Specify the layout to use when the list of choices appears.
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner.
+        // Apply the adapter to the spinner.
         spinner.setAdapter(adapter);
 
+        //Creating a calendar implementation for the date input for an event
+        EditText eDate = root.findViewById(R.id.editTextDate);
+        //Listens for when the user clicks on the EditText view. Focusable is disabled
+        //so this is how the user will interact w/ date editing.
+        eDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar mcurrentDate = Calendar.getInstance();
+                int mYear=mcurrentDate.get(Calendar.YEAR);
+                int mMonth=mcurrentDate.get(Calendar.MONTH);
+                int mDay=mcurrentDate.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog mDatePicker = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+                    public void onDateSet(DatePicker datepicker, int selectedYear, int selectedMonth, int selectedDay) {
+                        String selectedDate = (selectedMonth + 1) + "/" + selectedDay + "/" + selectedYear;
+                        eDate.setText(selectedDate);
+                    }
+                },mYear, mMonth, mDay);
+                mDatePicker.setTitle("Select event date");
+                mDatePicker.show();
+
+            }
+        });
         return root;
     }
 }
