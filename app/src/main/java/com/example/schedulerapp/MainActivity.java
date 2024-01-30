1975186cb80e5e0911a8314c628dae57e4904c7b
RolandHaden <davis.haden@gmail.com>
2024-01-30 13:40:39 -0500

    fixed boundary issues with events

--- app/src/main/java/com/example/schedulerapp/MainActivity.java
+++ app/src/main/java/com/example/schedulerapp/MainActivity.java
@@ -98,6 +98,7 @@ public class MainActivity extends AppCompatActivity {
             CalendarViewModel.getEventArrayList().clear();
             CalendarViewModel.getEventArrayList().addAll(loadedEventArrayList);
         }
+
     }
 
     private void saveCalendarDataToSharedPreferences() {
