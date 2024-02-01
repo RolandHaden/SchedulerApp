8a1a14218e2266ef6ebaf934844da541e9fd0400
RolandHaden <davis.haden@gmail.com>
2024-02-01 14:39:17 -0500

    classes now save when closing the app

--- app/src/main/java/com/example/schedulerapp/MainActivity.java
+++ app/src/main/java/com/example/schedulerapp/MainActivity.java
@@ -17,6 +17,8 @@ import com.example.schedulerapp.ui.checklist.ChecklistFragment;
 import com.example.schedulerapp.ui.checklist.ChecklistItem;
 import com.example.schedulerapp.ui.checklist.ChecklistViewModel;
 import com.example.schedulerapp.ui.profile.ProfileFragment;
+import com.example.schedulerapp.ui.profile.ProfileViewModel;
+import com.example.schedulerapp.ui.profile.classObject;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -54,6 +56,9 @@ public class MainActivity extends AppCompatActivity {
         // Restore data from SharedPreferences for ChecklistViewModel
         loadChecklistDataFromSharedPreferences();
 
+        // Restore data from SharedPreferences for ProfileViewModel
+        loadClassDataFromSharedPreferences();
+
         replaceFragment(new CalendarFragment());
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
@@ -87,8 +92,11 @@ public class MainActivity extends AppCompatActivity {
 
         // Save data to SharedPreferences for ChecklistViewModel
         saveChecklistDataToSharedPreferences();
-    }
 
+        // Save data to SharedPreferences for ProfileViewModel
+        saveClassDataToSharedPreferences();
+    }
+    //Loading Calendar Data
     private void loadCalendarDataFromSharedPreferences() {
         SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
         String eventsJson = sharedPreferences.getString("eventArrayList", "");
@@ -102,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
-
+    //Saving Calendar .
     private void saveCalendarDataToSharedPreferences() {
         SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
@@ -111,7 +119,7 @@ public class MainActivity extends AppCompatActivity {
         editor.putString("eventArrayList", eventsJson);
         editor.apply();
     }
-
+    //Loading Data from Checklist
     private void loadChecklistDataFromSharedPreferences() {
         SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
         String tasksJson = sharedPreferences.getString("taskArrayList", "");
@@ -124,13 +132,35 @@ public class MainActivity extends AppCompatActivity {
             ChecklistViewModel.getTaskArrayList().addAll(loadedTaskArrayList);
         }
     }
-
+    //Saving Data from Checklist
     private void saveChecklistDataToSharedPreferences() {
         SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
 
         String tasksJson = new Gson().toJson(ChecklistViewModel.getTaskArrayList());
         editor.putString("taskArrayList", tasksJson);
+        editor.apply();
+    }
+    //Load Data from Profile
+    private void loadClassDataFromSharedPreferences() {
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        String classJson = sharedPreferences.getString("classArrayList", "");
+
+        Type listType = new TypeToken<ArrayList<classObject>>() {}.getType();
+        ArrayList<classObject> loadedClassArrayList = new Gson().fromJson(classJson, listType);
+
+        if (loadedClassArrayList != null) {
+            ProfileViewModel.getClassArrayList().clear();
+            ProfileViewModel.getClassArrayList().addAll(loadedClassArrayList);
+        }
+    }
+    //Saving Data from Profile
+    private void saveClassDataToSharedPreferences() {
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        String classJson = new Gson().toJson(ProfileViewModel.getClassArrayList());
+        editor.putString("classArrayList", classJson);
         editor.apply();
     }
 }
